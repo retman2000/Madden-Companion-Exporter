7fc6db0f76d2a061dc437332133248ef7328841f
Garett Trent <garett.trent@gmail.com>
2021-11-15 23:20:32 -0500

    Update app.js with firebase info.

--- app.js
+++ app.js
@@ -10,15 +10,15 @@ const app = express();
 // Enter the path to your service account json file below where it says "REPLACE_WITH_SERVICE_ACCOUNT"
 // If you need more help with this step go here: https://firebase.google.com/docs/admin/setup
 
-// const serviceAccount = require("./REPLACE_WITH_SERVICE_ACCOUNT.json");
+const serviceAccount = require("./game-time-3ac5d-firebase-adminsdk-uln0d-da21a4d97a.json");
 
 // TODO: Uncomment out line 17-21
 // Enter your database url from firebase where it says <DATABASE_NAME> below.
 // Refer to picture for reference. It's the 2nd property.
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://<DATABASE_NAME>.firebaseio.com/"
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://game-time-3ac5d-default-rtdb.firebaseio.com/"
+});
 
 app.set('port', (process.env.PORT || 3001));
 
