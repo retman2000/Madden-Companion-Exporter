bb2a5dbe5c7230a06270970d7d7c844aa1759b05
Garett Trent <garett.trent@gmail.com>
2021-11-17 22:28:33 -0500

    Add test post 3

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.set('port', (process.env.PORT || 3001));
 
 app.get('*', (req, res) => {
     console.log('Madden Data is running on port', app.get('port'))
-    res.send('Madden Companion Exporter ');
+    res.send(app.get('port'));
 });
 
 app.post('/:username/:platform/:leagueId/test', (req, res) => {
